fede61c892bc5f8a8cd8dc9020f0cb498ff11a7e
Mohammad Ali <134193466+mdalii69@users.noreply.github.com>
2025-01-24 20:48:07 +0530

    Login using credentials
    Here we send credentials as keys and login in a website

---
+++ basic-Login.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+username = "standard_user"
+password = "secret_sauce"
+login_url = "https://www.saucedemo.com/"
+driver.get(login_url)
+
+username_field = driver.find_element(By.ID, "user-name")
+password_field = driver.find_element(By.ID, "password")
+
+username_field.send_keys(username)
+password_field.send_keys(password)
+
+login_button = driver.find_element(By.ID, "login-button")
+assert not login_button.get_attribute("disabled")
+login_button.click()
+
+success_element = driver.find_element(By.CSS_SELECTOR, ".title")
+
+try:
+    assert success_element.text == "Products"
+    print("Test Passed: Login Successful!")
+except AssertionError:
+    print("Test Failed: Login Failed!")
+
+driver.quit()
